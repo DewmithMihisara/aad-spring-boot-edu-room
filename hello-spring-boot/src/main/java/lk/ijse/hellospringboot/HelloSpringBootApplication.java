1f04d125a69892701471951f6dd077c5eb3a3803
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-04 16:34:21 +0530

    customer crud done in spring boot

--- hello-spring-boot/src/main/java/lk/ijse/hellospringboot/HelloSpringBootApplication.java
+++ hello-spring-boot/src/main/java/lk/ijse/hellospringboot/HelloSpringBootApplication.java
@@ -1,13 +1,20 @@
 package lk.ijse.hellospringboot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
