0894aaf4a4aa7f496db515d1510c7b728c4c5003
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-18 09:53:25 +0530

    add security project

---
+++ spring-boot-with-basic-auth/src/main/java/lk/ijse/springbootwithbasicauth/SpringBootWithBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.springbootwithbasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithBasicAuthApplication.class, args);
+	}
+
+}
---
+++ spring-boot-with-basic-auth/src/test/java/lk/ijse/springbootwithbasicauth/SpringBootWithBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.springbootwithbasicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithBasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
