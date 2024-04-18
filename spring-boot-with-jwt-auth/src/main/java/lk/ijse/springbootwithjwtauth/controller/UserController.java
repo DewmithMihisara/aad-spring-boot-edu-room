15b6ae74b8c75dd8e935ef90e6490912a3cf95e5
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-18 12:16:18 +0530

    add user controller

---
+++ spring-boot-with-jwt-auth/src/main/java/lk/ijse/springbootwithjwtauth/controller/UserController.java
@@ -0,0 +1,15 @@
+package lk.ijse.springbootwithjwtauth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-04-18
+ * @since 0.0.1
+ */
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+}
