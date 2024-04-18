bd8ed12cf57f042ce1c96e71b05b4dacc6d958c9
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-18 10:54:00 +0530

    add security config

---
+++ spring-boot-with-basic-auth/src/main/java/lk/ijse/springbootwithbasicauth/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package lk.ijse.springbootwithbasicauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-04-18
+ * @since 0.0.1
+ */
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
